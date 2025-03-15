eebe1a384494e4938a997ec7960a3df5260e488d
Martynas <martynas.bartnykas@gmail.com>
2025-03-15 14:13:16 +0200

    Added clickable button which changes TextView field content once pressed.

--- app/src/main/java/com/example/firstpracticetask_20250315/MainActivity.java
+++ app/src/main/java/com/example/firstpracticetask_20250315/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.firstpracticetask_20250315;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnChangeText = findViewById(R.id.btnChangeText);
+        TextView tvMain = findViewById(R.id.tvMain);
+
+        btnChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMain.setText(getString(R.string.button_clicked_text));
+            }
+        });
+
+
+        }
     }
-}
